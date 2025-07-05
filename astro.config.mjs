4559718e08c37652ee1052b03fa893a9b6148bad
Mykola Gurov <ngurov@gmail.com>
2025-07-05 08:32:28 +0200

    presi is the base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://mgurov.github.io',
+    base: '/presi',
+    trailingSlash: 'always',
+});
