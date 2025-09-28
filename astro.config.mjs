d173f3d11490b4c6af1586c5e5054695f19ae714
Mykola Gurov <ngurov@gmail.com>
2025-09-28 07:28:09 +0200

    lokito on framer motion experimentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mgurov.github.io',
-    base: '/presi',
-    trailingSlash: 'always',
-    redirects: {
-    "/presi/01-tip/": "/presi/presi/01-tip/dev/",
-  }
+  site: 'https://mgurov.github.io',
+  base: '/presi',
+  trailingSlash: 'always',
+
+  redirects: {
+  "/presi/01-tip/": "/presi/presi/01-tip/dev/",
+},
+
+  integrations: [react()]
 });
